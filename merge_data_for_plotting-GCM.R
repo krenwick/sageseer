a1d99b3c92515b659fe51d99587a1fb0edf33967
krenwick <katie.renwick@gmail.com>
2016-09-27 08:47:37 -0600

    added code to clean Carolines data and complete merge

--- merge_data_for_plotting-GCM.R
+++ merge_data_for_plotting-GCM.R
@@ -12,21 +12,31 @@ dpath <- "/Users/poulterlab1/Box Sync/sageseer/ModelComparison/Indiv_model_outpu
 
 # load plot list w/ clim data + site list with Katie's odd coords
 clim_dat <- read.csv(paste(dpath, "../focal_sites_for_comparison.csv", sep=""))
+c2 <- select(clim_dat,longitude:site) # need for merging
 
 # load data from each model, mutate to make formats consistent
-andy <- read.csv(paste(dpath, "AK_cover_GCM_predictions.csv",sep="")) %>%
+andy <- read.csv(paste(dpath, "AK_cover_GCM_predictions_with_sites.csv",
+                       sep="")) %>%
   na.omit() %>%
   filter(baseline<6.8 & baseline>6) %>%
-  distinct()
+  distinct() %>%
+  select(-X)
 katie <- read.csv(paste(dpath, "KMR_cover_GCM-fullmodel.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
-caroline <- read.csv(paste(dpath, "focal_sites_for_comparison_CCrandfor_extract_new7-18.csv", sep=""))
+RF <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_RandforCorrectedoutput.csv", sep="")) %>%
+  rename(longitude=lon,latitude=lat)
+Maxraw <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_maxentRawoutput.csv", sep="")) %>%
+  rename(longitude=lon,latitude=lat)
+Maxbin <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_maxentBinaryoutput.csv", sep="")) %>%
+  mutate(model="MaxEntBin") %>%
+  rename(longitude=lon,latitude=lat)
+
 daniel <- read.csv(paste(dpath, "DRS_726focusSites_recruitment_predictions_DaymetCorrected.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
 
 # combine all into one data frame 
 # FIRST: get all disparate data in the same format
-c2 <- select(clim_dat,longitude:site) 
+
 car2 <- select(caroline,longitude:site,model:projected) %>%
   rename(predicted=projected)
 k2 <- merge(katie, ksites, by=c("longitude","latitude")) %>%
@@ -35,20 +45,14 @@ d2 <- merge(daniel,c2, by="site",all.x=T, all.y=F) %>%
   select(longitude,latitude,site,model, var,mag,baseline,predicted)
 
 # SECOND: rbind to combine them all
-all <- rbind(d2,andy,k2,car2)
+all <- rbind(katie,andy,RF,Maxraw,Maxbin)
 
-# THIRD: fix inconsistencies in var/mag coding
-all2 <- mutate(all, var=ifelse(var=="tmax"|var=="tmean", "temp","ppt")) %>%
-  mutate(mag=ifelse(mag==-.1,.9,mag)) %>%
-  mutate(mag=ifelse(mag==.1,1.1,mag)) %>%
-  mutate(mag=ifelse(mag==.2&var=="ppt",1.2,mag)) %>%
-  na.omit()
 # check- should have same # in each category
-table(all2$var, all2$mag, all2$model)
+table(all$scenario, all$GCM, all$model)
 
 # merge in the climate covariates
-all3 <- merge(all2,clim_dat, by="site")
-table(all3$var, all3$mag, all3$model)
+all3 <- merge(all,clim_dat, by="site")
+table(all3$scenario, all3$GCM, all3$model)
 
 # scale MAT so is degrees (original data lacked decimal)
 all4 <- mutate(all3, MAT=MAT/10)
