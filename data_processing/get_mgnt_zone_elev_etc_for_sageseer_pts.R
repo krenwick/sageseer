82f5fc2ceebd71252f667422f71d10d035bdead7
krenwick <katie.renwick@gmail.com>
2016-10-05 16:46:10 -0600

    added code to pull in RR class data for points

--- data_processing/get_mgnt_zone_elev_etc_for_sageseer_pts.R
+++ data_processing/get_mgnt_zone_elev_etc_for_sageseer_pts.R
@@ -5,15 +5,16 @@
 # Output: writes csv of points with additional attributes
 #############################################################################
 # libraries
+rm(list=ls())
 library(raster)
 require("rgdal") # requires sp, will use proj.4 if installed
 require("maptools")
-require("ggplot2")
-require("plyr")
+#require("ggplot2")
+#require("plyr")
 
 # Read in sageseer points
 pts <- read.csv("/Users/poulterlab1/Box Sync/sageseer/ModelComparison/focal_sites_for_comparison.csv")
-# load states map to overlay
+# load states map to overlay (ALSO: need for correct CRS info)
 states <- readOGR("/Users/poulterlab1/Documents/GIS_baselayers/", "states")
 wus <- states[states$STATE_ABBR=="WA"|states$STATE_ABBR=="OR"|states$STATE_ABBR=="CA"
               |states$STATE_ABBR=="ID"|states$STATE_ABBR=="NV"|states$STATE_ABBR=="MT"
@@ -38,20 +39,20 @@ dem1 <-  raster(paste(edir,"gt30w140n40.tif", sep=""))
 dem2 <-  raster(paste(edir,"gt30w140n90.tif", sep=""))
 dem <- merge(dem1,dem2)
 
-# convert to points for plotting
-val1 <- rasterToPoints(dem)
-val2 <- data.frame(val1)
+# convert to points for plotting (takes a while, do once to check)
+#val1 <- rasterToPoints(dem)
+#val2 <- data.frame(val1)
 
-# Get kuchler data in plottable format
-artrdist@data$id = rownames(artrdist@data)
-kuch.pt <- fortify(artrdist, region="id")
-kuch.df = join(kuch.pt, artrdist@data, by="id")
+# Get kuchler data in plottable format (this is just a check)
+#artrdist@data$id = rownames(artrdist@data)
+#kuch.pt <- fortify(artrdist, region="id")
+#kuch.df = join(kuch.pt, artrdist@data, by="id")
 ggplot(data=pts, aes(x=longitude, y=latitude)) +
  # geom_raster(data=val2,aes(x,y)) + # takes long ime to draw raster
   #geom_polygon(data=grouse, aes(long,lat, group), fill=NA,color="green") +
   geom_polygon(data=kuch.df, aes(long,lat, fill=as.factor(KUCHLER_),group=group), color=NA) +
   geom_point(data=pts,aes(shape=as.factor(outlier))) +
-  geom_polygon(data=wus, aes(long,lat, group),fill=NA, color="grey46") +
+  geom_polygon(data=wus, aes(long,lat, group),fill=77*NA, color="grey46") +
   theme_bw(base_size = 24, base_family = "Helvetica") +
   xlab("Longitude") +
   ylab("Latitude") +
@@ -62,18 +63,24 @@ sdir <- ("/Users/poulterlab1/Documents/GIS_baselayers/")
 ecoreg <- readOGR(sdir,"us_eco_l3")
 ecoreg <- spTransform(ecoreg, CRS(proj4string(states)))
 
+#load SSURGO soil temp and moisture regime classes
+ss1 <- read.csv("/Users/poulterlab1/Box Sync/sageseer/ModelComparison/focal_pts_project_extract_all_SMTRegime.csv")
+ss2 <- merge(pts,ss1,by="site", all.X=T)
+
+
 # Extract management unit and elev by points
 pts2 <- pts[,1:2]
 elev <- raster::extract(dem,pts2)
-mgnt.unit <- extract(grouse,pts2) %>%
+mgnt.unit <- raster::extract(grouse,pts2) %>%
   dplyr::select(Mgmt_zone:Name)
-eco <- extract(ecoreg,pts2) %>%
+eco <- raster::extract(ecoreg,pts2) %>%
   dplyr::select(US_L3CODE:NA_L1NAME)
-sagetype <- extract(artrdist, pts2) %>%
+sagetype <- raster::extract(artrdist, pts2) %>%
   dplyr::select(VEGTYP_LAB)
-KL2 <- extract(KL, pts2)
+KL2 <- raster::extract(KL, pts2)
 KL3 <- KL2 %>%  dplyr::select(PNV_GROUPS)
+ss3 <- merge(pts2,ss1,by=c("longitude","latitude"), all=T)
 
-newout <- cbind(pts,elev,mgnt.unit,eco,sagetype,KL3) 
+newout <- cbind(pts,elev,mgnt.unit,eco,sagetype,KL3,ss3) 
 write.csv(newout,"/Users/poulterlab1/Box Sync/sageseer/ModelComparison/focal_sites_by_zone.csv", row.names=F)
 
