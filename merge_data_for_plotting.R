09a3a7823f94894abc34e985fe8ce74b8aae9145
krenwick <katie.renwick@gmail.com>
2016-12-06 21:33:11 -0700

    changed read-in and manipulation to deal with new data stucture in Caroline's new output

--- merge_data_for_plotting.R
+++ merge_data_for_plotting.R
@@ -17,29 +17,37 @@ ksites <- read.csv(paste(dpath, "../KMR_sitelist2.csv", sep=""))
 # load data from each model, mutate to make formats consistent
 andy <- read.csv(paste(dpath, "AK_cover_predictions.csv",sep="")) %>%
   na.omit() %>%
-  filter(baseline<6.8 & baseline>6) %>%
-  distinct()
-katie <- read.csv(paste(dpath, "KMR_cover-fullmodel.csv", sep="")) %>%
+  group_by(model,site,var,mag) %>%
+  summarise_each(funs(mean)) %>%
+  select(longitude,latitude,site,model,var,mag,baseline,predicted)
+katie <- read.csv(paste(dpath, "KMR_cover-fullmodel_noSSM_400ppm.csv", sep="")) %>%
   mutate(longitude=round(longitude,3), latitude=round(latitude,3)) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
-caroline <- read.csv(paste(dpath, "focal_sites_for_comparison_CCrandfor_extract_new7-18.csv", sep=""))
+caroline <- read.csv(paste(dpath, "focal_sites_for_comparison_rmvAZpts_PERTURBclim_randfor_CAC.csv", sep=""))
 daniel <- read.csv(paste(dpath, "DRS_GISSM-output_726AKpg_Exp1-Sensitivity_DaymetCorrected.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
 
 # combine all into one data frame 
 # FIRST: get all disparate data in the same format
-c2 <- select(clim_dat,longitude:site) 
-car2 <- select(caroline,longitude:site,model:projected) %>%
-  rename(predicted=projected)
+car2 <- select(caroline,lon,lat,site,model,scenario,baseline,predicted) %>%
+  rename(longitude=lon,latitude=lat) %>%
+  mutate(scenario2=ifelse(scenario=="02inctemp","tmean_0.2","fix")) %>%
+  mutate(scenario2=ifelse(scenario=="2inctemp","tmean_2.0",scenario2)) %>%
+  mutate(scenario2=ifelse(scenario=="4inctemp","tmean_4.0",scenario2)) %>%
+  mutate(scenario2=ifelse(scenario=="ppt10dec","ppt_0.9",scenario2)) %>%
+  mutate(scenario2=ifelse(scenario=="ppt10inc","ppt_1.1",scenario2)) %>%
+  mutate(scenario2=ifelse(scenario=="ppt20inc","ppt_1.2",scenario2)) %>%
+  separate(scenario2,c("var","mag"),sep="_") %>%
+  select(-scenario) %>%
+  select(longitude:model,var,mag,baseline,predicted)
 k2 <- merge(katie, ksites, by=c("longitude","latitude")) %>%
   select(longitude,latitude,site,model, var,mag,baseline,predicted)
-d2 <- daniel %>% select(-X, -longitude, -latitude) %>%
-  merge(c2, by="site",all.x=T, all.y=F) %>%
+d2 <- daniel %>% select(-X) %>%
   select(longitude,latitude,site,model, var,mag,baseline,predicted) %>%
   mutate(model="DRS")
 
 # SECOND: rbind to combine them all
-all <- rbind(d2,andy,k2,car2)
+all <- rbind.data.frame(d2,andy,k2,car2)
 
 # THIRD: fix inconsistencies in var/mag coding
 all2 <- mutate(all, var=ifelse(var=="tmax"|var=="tmean", "temp","ppt")) %>%
@@ -54,9 +62,19 @@ table(all2$var, all2$mag, all2$model)
 all3 <- merge(all2,clim_dat, by="site")
 table(all3$var, all3$mag, all3$model)
 
-# scale MAT so is degrees (original data lacked decimal)
-all4 <- mutate(all3, MAT=MAT/10)
+# check which sites caroline eliminated
+site <- seq(1,726,1)
+name <- rep("fake",726)
+s <- cbind.data.frame(name,site)
+c2 <- select(car2,site,predicted) %>%
+  group_by(site) %>%
+  summarise(predicted=mean(predicted))
+check <- merge(s,c2,by="site",all=T)
+bad <- c(495,496,497,498,580,581,583,632,633,634,668) # it matches- yay!
+
+# Eliminate "bad" sites, including ND point Katie doesn't have
+all4 <- filter(all3, site %in% bad==FALSE)
 
 # output merged data
-write.csv(all4, (paste(dpath, "merged_data-co2.csv", sep="")), row.names=F)
+write.csv(all4, (paste(dpath, "../merged_data-co2.csv", sep="")), row.names=F)
 
