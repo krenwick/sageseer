0cbff797ff2f9159295ff99959b509c003499359
krenwick <katie.renwick@gmail.com>
2016-12-06 21:34:53 -0700

    change processing of Andys output so average cover from diff subspecies at same plot instread of simply using wyomingensis

--- merge_data_for_plotting-GCM.R
+++ merge_data_for_plotting-GCM.R
@@ -17,10 +17,11 @@ c2 <- select(clim_dat,longitude:site) # need for merging
 # FIRST: load data from each model, mutate to make formats consistent
 andy <- read.csv(paste(dpath, "AK_cover_GCM_predictions_with_sites.csv",
                        sep="")) %>%
-  na.omit() %>%
-  filter(baseline<6.8 & baseline>6) %>%
-  distinct() %>%
-  select(-X)
+  select(-X) %>%
+  group_by(model,site,scenario,GCM) %>%
+  summarise_each(funs(mean)) %>%
+  select(model,site,longitude,latitude,baseline,predicted,scenario,GCM) 
+a2 <- data.frame(andy)
 katie <- read.csv(paste(dpath, "KMR_cover_GCM-fullmodel.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
 RF <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_RandforCorrectedoutput.csv", sep="")) %>%
@@ -36,7 +37,8 @@ daniel <- read.csv(paste(dpath, "DRS_GISSM-output_726AKpg_Exp2-GCM-Scenarios_Day
   mutate(scenario=gsub("RCP", "rcp",scenario)) 
 
 # SECOND: rbind to combine them all
-all <- rbind(katie,andy,RF,Maxraw,Maxbin,daniel)
+all <- rbind(katie,a2,RF,Maxraw,Maxbin,daniel)
+tail(all)
 
 # check- should have same # in each category
 table(all$scenario, all$GCM, all$model)
@@ -46,7 +48,7 @@ all3 <- merge(all,clim_dat, by="site")
 table(all3$scenario, all3$GCM, all3$model)
 
 # scale MAT so is degrees (original data lacked decimal)
-all4 <- mutate(all3, MAT=MAT/10)
+all4 <- mutate(all3, MAT=bio1/10)
 
 # output merged data
 write.csv(all4, (paste(dpath, "../merged_data_GCM.csv", sep="")), row.names=F)
