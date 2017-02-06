4caa2f4866a8031a6ac8a5ffca3f4dcc4be79661
krenwick <katie.renwick@gmail.com>
2017-02-06 13:09:31 -0700

    updates to use Caroline's new un-corrected data

--- merge_data_for_plotting.R
+++ merge_data_for_plotting.R
@@ -7,10 +7,16 @@ rm(list=ls())
 library(dplyr)
 library(tidyr)
 
-# set file path for sageseer- CHANGE BASED ON YOUR COMPUTER
-dpath <- "/Users/poulterlab1/Box Sync/sageseer/ModelComparison/Indiv_model_output/"
+# List of "bad" sites
+bad <- c(495,496,497,498,580,581,583,632,633,634,668,62)
 
-# load plot list w/ clim data + site list with Katie's odd coords
+# set file path for sageseer- CHANGE BASED ON YOUR COMPUTER
+setwd("/Users/poulterlab1/version-control/sageseer/")
+
+# folder path:
+dpath <- "data/indiv_model_output/"
+
+# load plot list w/ clim data
 clim_dat <- read.csv(paste(dpath, "../focal_sites_for_comparison.csv", sep=""))
 ksites <- read.csv(paste(dpath, "../KMR_sitelist2.csv", sep=""))
 
@@ -23,7 +29,7 @@ andy <- read.csv(paste(dpath, "AK_cover_predictions.csv",sep="")) %>%
 katie <- read.csv(paste(dpath, "KMR_cover-fullmodel_noSSM_400ppm.csv", sep="")) %>%
   mutate(longitude=round(longitude,3), latitude=round(latitude,3)) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
-caroline <- read.csv(paste(dpath, "focal_sites_for_comparison_rmvAZpts_PERTURBclim_randfor_CAC.csv", sep=""))
+caroline <- read.csv(paste(dpath, "RF_perturb_rawoutput2.csv", sep=""))
 daniel <- read.csv(paste(dpath, "DRS_GISSM-output_726AKpg_Exp1-Sensitivity_DaymetCorrected.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
 
@@ -56,25 +62,15 @@ all2 <- mutate(all, var=ifelse(var=="tmax"|var=="tmean", "temp","ppt")) %>%
   mutate(mag=ifelse(mag==.2&var=="ppt",1.2,mag)) %>%
   na.omit()
 # check- should have same # in each category
-table(all2$var, all2$mag, all2$model)
+#table(all2$var, all2$mag, all2$model)
 
 # merge in the climate covariates
 all3 <- merge(all2,clim_dat, by="site")
-table(all3$var, all3$mag, all3$model)
-
-# check which sites caroline eliminated
-site <- seq(1,726,1)
-name <- rep("fake",726)
-s <- cbind.data.frame(name,site)
-c2 <- select(car2,site,predicted) %>%
-  group_by(site) %>%
-  summarise(predicted=mean(predicted))
-check <- merge(s,c2,by="site",all=T)
-bad <- c(495,496,497,498,580,581,583,632,633,634,668) # it matches- yay!
 
 # Eliminate "bad" sites, including ND point Katie doesn't have
 all4 <- filter(all3, site %in% bad==FALSE)
+table(all4$var, all4$mag, all4$model) # Check- should be 714
 
 # output merged data
-write.csv(all4, (paste(dpath, "../merged_data-co2.csv", sep="")), row.names=F)
+write.csv(all4, (paste(dpath, "../merged_data_perturb.csv", sep="")), row.names=F)
 
