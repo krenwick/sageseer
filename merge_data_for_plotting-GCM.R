67bec0b3dec62eb04d41269b5ea865100a46a515
krenwick <katie.renwick@gmail.com>
2017-02-06 11:23:27 -0700

    updated code to read in new RF output that has been corrected

--- merge_data_for_plotting-GCM.R
+++ merge_data_for_plotting-GCM.R
@@ -1,18 +1,21 @@
 #############################################################################
 # use coordinates from Andy's project to extract data
 # ouput csv with data in common format
-# Sept. 2017: updates to file paths due to file reorganization
+# Sept. 2016: updates to file paths due to file reorganization
+# Jan 2017: more minor updates to read in new RF
 #############################################################################
 rm(list=ls())
 library(dplyr)
 library(tidyr)
 
-# set file path for sageseer- CHANGE BASED ON YOUR COMPUTER
-dpath <- "/Users/poulterlab1/Box Sync/sageseer/ModelComparison/Indiv_model_output/"
+# List of "bad" sites
+bad <- c(495,496,497,498,580,581,583,632,633,634,668,62)
 
-# load plot list w/ clim data + site list with Katie's odd coords
-clim_dat <- read.csv(paste(dpath, "../focal_sites_for_comparison.csv", sep=""))
-c2 <- select(clim_dat,longitude:site) # need for merging
+# set file path for sageseer- CHANGE BASED ON YOUR COMPUTER
+setwd("/Users/poulterlab1/version-control/sageseer/")
+
+# folder path:
+dpath <- "data/Indiv_model_output/"
 
 # FIRST: load data from each model, mutate to make formats consistent
 andy <- read.csv(paste(dpath, "AK_cover_GCM_predictions_with_sites.csv",
@@ -24,32 +27,46 @@ andy <- read.csv(paste(dpath, "AK_cover_GCM_predictions_with_sites.csv",
 a2 <- data.frame(andy)
 katie <- read.csv(paste(dpath, "KMR_cover_GCM-fullmodel.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100)
-RF <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_RandforCorrectedoutput.csv", sep="")) %>%
-  rename(longitude=lon,latitude=lat)
-Maxraw <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_maxentRawoutput.csv", sep="")) %>%
-  rename(longitude=lon,latitude=lat)
-Maxbin <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_maxentBinaryoutput.csv", sep="")) %>%
-  mutate(model="MaxEntBin") %>%
-  rename(longitude=lon,latitude=lat)
+RF <- read.csv(paste(dpath, "RF_allmodels.csv", sep="")) %>%
+  mutate(GCM2=ifelse(GCM=="ccsm","CCSM4","fix")) %>%
+  mutate(GCM2=ifelse(GCM=="cesm","CESM1-CAM5",GCM2)) %>%
+  mutate(GCM2=ifelse(GCM=="giss","GISS-E2-H-CC",GCM2)) %>%
+  mutate(GCM2=ifelse(GCM=="hgem","HadGEM2-AO",GCM2)) %>%
+  mutate(GCM2=ifelse(GCM=="mpi","MPI-ESM-LR",GCM2)) %>%
+  select(-GCM) %>%
+  rename(GCM=GCM2)
+#Maxraw <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_maxentRawoutput.csv", sep="")) %>%
+ # rename(longitude=lon,latitude=lat)
+#Maxbin <- read.csv(paste(dpath, "focal_sites_for_comparison_CAC_maxentBinaryoutput.csv", sep="")) %>%
+ # mutate(model="MaxEntBin") %>%
+  #rename(longitude=lon,latitude=lat)
 daniel <- read.csv(paste(dpath, "DRS_GISSM-output_726AKpg_Exp2-GCM-Scenarios_DaymetCorrected.csv", sep="")) %>%
   mutate(baseline=baseline*100,predicted=predicted*100) %>%
   select(-X) %>%
   mutate(scenario=gsub("RCP", "rcp",scenario)) 
 
 # SECOND: rbind to combine them all
-all <- rbind(katie,a2,RF,Maxraw,Maxbin,daniel)
+k_RF <- katie %>% select(site,longitude,latitude) %>%
+  group_by(site) %>% summarise_each(funs(mean)) %>%
+   merge(RF, by="site") %>%
+  select(model, site:GCM)
+nrow(k_RF)
+all <- rbind(katie,a2,k_RF,daniel) #,Maxraw,Maxbin
 tail(all)
 
-# check- should have same # in each category
-table(all$scenario, all$GCM, all$model)
+# Eliminate "bad" sites 
+# (problem in initial presence data from which focal sites chosen)
+all2 <- filter(all, site %in% bad==FALSE)
+
+# check- should have 714 in each category
+table(all2$scenario, all2$GCM, all2$model)
 
 # merge in the climate covariates
-all3 <- merge(all,clim_dat, by="site")
-table(all3$scenario, all3$GCM, all3$model)
-
-# scale MAT so is degrees (original data lacked decimal)
-all4 <- mutate(all3, MAT=bio1/10)
+# load plot list w/ clim data
+clim_dat <- read.csv(paste(dpath, "../focal_sites_for_comparison.csv", sep=""))
+all3 <- merge(all2,clim_dat, by="site")
+#table(all3$scenario, all3$GCM, all3$model)
 
 # output merged data
-write.csv(all4, (paste(dpath, "../merged_data_GCM.csv", sep="")), row.names=F)
+write.csv(all3, (paste(dpath, "../merged_data_GCM.csv", sep="")), row.names=F)
 
